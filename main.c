a87b15b2cefc6f92532b14ce18c8376a8a1aa393
Shoorano <danwilloughby2@gmail.com>
2025-08-10 19:54:06 +0100

    Added main.c.

---
+++ main.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main() {
+    int current, prev, prev_prev;
+    while ((current = getchar()) != EOF) {
+        if (prev_prev == ':' && prev == 'q') {
+            break;
+        }
+    }
+}
